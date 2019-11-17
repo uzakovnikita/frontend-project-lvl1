b34f9fa2fcfd57609b95bd95a4eb68fb6f7d6a08
uzakovnikita <nikitauzakov2@gmail.com>
2019-11-18 01:49:48 +0400

    fix engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,10 +1,10 @@
 import { hello, succesfulResult, question, personalHello } from "."
 
-export const engine = (random, rules, description, counter = 0) => {
+export const engine = (random, rules, description, counter = 0, nikname) => {
     if (counter == 0) {
         hello();
         description();
-        var name = personalHello();
+        nikname = personalHello();
     }
     let quest = random();
     let answerUser = question(quest);
@@ -12,11 +12,14 @@ export const engine = (random, rules, description, counter = 0) => {
     if (answerTrue == answerUser && counter < 2) {
         console.log('Correct!');
         counter += 1;
-        return engine(random, rules, description, counter);
-    } else if (answerTrue !== answerUser) {
+        return engine(random, rules, description, counter, nikname);
+    } else if (answerTrue != answerUser) {
+        console.log(`${answerUser} answerUser`);
+        console.log(`${answerTrue} answerTrue`);
+        console.log(answerTrue == answerUser);
         console.log(`${answerUser} is wrong answer ;(. Correct answer was ${answerTrue}.`)
     } else if (counter == 2) {
-        succesfulResult(name);
+        succesfulResult(nikname);
     }
 
 }
