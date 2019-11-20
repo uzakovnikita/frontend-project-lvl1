29634f12bf0fb4f3b5e58fa198b8a03f806d6f65
uzakovnikita <nikitauzakov2@gmail.com>
2019-11-20 22:01:49 +0400

    add brain-progresion

--- src/games/progressionRules.js
+++ src/games/progressionRules.js
@@ -1,4 +1,4 @@
-import { getRandom, searchOne, searchTwo, getRandomTen, searchNext, searchEndOne, reverse, searchEndNext } from '..';
+import { getRandom, getRandomTen } from '..';
 
 export const random = () => {
     let a = getRandom();
@@ -25,24 +25,16 @@ export const random = () => {
     return result;
 }
 export const rules = (str) => {
-    // let i = 0;
-    // while (str[i] !== '.' && str[i + 1] !== '.') {
-    //     i += 1;
-    // }
-    // if (i > str.length / 2) {
-    //     let one = searchOne(str);
-    //     let next = searchNext(str);
-    // } else if (i <= str.length / 2) {
-    //     let next = reverse(searchEndOne(str));
-    //     let one = reverse(searchEndNext(str));
-    // }
     let arr = str.split(' ');
     let index = arr.indexOf('..');
     let d = 0;
     let result = 0;
-    if (index > arr.length / 2) {
+    if (index > (arr.length) / 2) {
         d = arr[1] - arr[0];
-        result = arr[0] + d * index;
+        result = +arr[0] + d * index;
+    } else {
+        d = arr[9] - arr[8];
+        result = +arr[9] - d * (arr.length - index - 2);
     }
     return result;
 }
