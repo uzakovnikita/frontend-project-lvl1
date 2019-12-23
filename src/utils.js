439e86072d525ed046d09fccbf7cf74cc218cdcc
uzakovnikita <nikitauzakov2@gmail.com>
2019-12-24 00:13:22 +0400

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-const getRandom = (min, max) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
-export default getRandom;
+export default (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
